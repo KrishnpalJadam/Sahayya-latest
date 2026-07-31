e7471ec7dcf7901a778db7c8b4d4db109ecaa83a
Ahmed Bilal Khan <ahmedbilalkhangl09@gmail.com>
2026-07-31 19:21:28 +0500

    chore: remove unused useMemo import from TicketDetail

--- src/Screens/Support/TicketDetail.js
+++ src/Screens/Support/TicketDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, ScrollView, ActivityIndicator, KeyboardAvoidingView, Platform, TextInput, Keyboard } from 'react-native';
 import CommanView from '../../Component/CommanView';
 import HeaderForUser from '../../Component/HeaderForUser';
