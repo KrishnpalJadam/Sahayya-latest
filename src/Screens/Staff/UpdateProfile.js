83844bc7945e27b596743ee710c02bffc907a583
Ahmed Bilal Khan <ahmedbilalkhangl09@gmail.com>
2026-08-20 17:59:01 +0500

    Remove Working Hours field from Last Work Experience in Edit Profile

--- src/Screens/Staff/UpdateProfile.js
+++ src/Screens/Staff/UpdateProfile.js
@@ -349,24 +349,6 @@ const UpdateProfile = forwardRef((props, ref) => {
         error={errors.salary}
       />
       
-      <DropdownComponent 
-        title={'Working Hours'} 
-        placeholder={'Select working hours'}
-        data={Array.from({ length: 24 }, (_, i) => ({
-          label: `${i + 1} Hour${i === 0 ? '' : 's'}`,
-          value: `${i + 1}`
-        }))}
-        value={workingHours}
-        onChange={(item) => {
-          setWorkingHours(item.value);
-          if (errors.workingHours) setErrors({...errors, workingHours: null});
-        }}
-        error={errors.workingHours}
-        width={'100%'}
-        style_dropdown={{ marginHorizontal: 0 }}
-        marginHorizontal={0}
-      />
-      
       <Input 
         title={'Household Owner Name'} 
         value={ownerName}
