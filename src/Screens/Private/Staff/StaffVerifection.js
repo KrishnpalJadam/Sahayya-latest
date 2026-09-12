e100a1d044802667250c77102018ced0e6c22055
Ahmed Bilal Khan <ahmedbilalkhangl09@gmail.com>
2026-09-12 13:19:03 +0500

    fix: add missing Platform import in Aadhar.js and StaffVerifection.js causing app crash on Aadhaar OTP screen

--- src/Screens/Private/Staff/StaffVerifection.js
+++ src/Screens/Private/Staff/StaffVerifection.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, TouchableOpacity, Platform } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import CommanView from '../../../Component/CommanView';
 import Typography from '../../../Component/UI/Typography';
